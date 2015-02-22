16072290df586f65c351eb2433b6d1a53bc03cda
Dmitry Golofaev <mistday@yandex.ru>
2015-02-22 14:20:45 +0300

    add begin config for stylus task

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,14 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    
+    stylus: {
+      compile: {
+        files: {
+          'css/style.css': 'styl/main.styl'
+        }
+      }
+    }
   });
+
+
+  grunt.loadNpmTasks('grunt-contrib-stylus');
 }
