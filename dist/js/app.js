574661bb43a239f43855e9c06c03adb5cdc1f6ee
Dmitry <mistday@yandex.ru>
2015-02-24 10:27:46 +0300

    add imagemin, coffee and change directory
    delete img
    
    last delete

---
+++ dist/js/app.js
@@ -0,0 +1,6 @@
+(function() {
+  main(function() {
+    return 1 + 1;
+  });
+
+}).call(this);
--- gruntfile.js
+++ gruntfile.js
@@ -3,15 +3,40 @@ module.exports = function(grunt) {
     stylus: {
       compile: {
         files: {
-          'css/style.css': 'styl/main.styl'
+          'dist/css/style.css': 'src/styl/main.styl'
         },
         options: {
           compress: true
         }
+      }
+    },
+    imagemin: {
+      dynamic: {
+        options: {
+          optimixationLevel: 3
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/img/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'dist/img'
+        }]
+      }
+    },
+    coffee: {
+      glob_to_multiple: {
+        expand: true,
+        flatten: true,
+        cwd: 'src/coffee/',
+        src: ['*.coffee'],
+        dest: 'dist/js/',
+        ext: '.js'
       }
     }
   });
 
 
   grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
 }
